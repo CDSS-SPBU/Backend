ec9a3f237ecbb84f7c25fb9f66c5198c230cb57c
Approbations <appolonina2005@gmail.com>
2025-11-18 15:34:38 +0600

    Добавление взаимодейтсвия с frontend

--- api/router_socket.py
+++ api/router_socket.py
@@ -1,10 +1,13 @@
 import fastapi
 import json
+import requests
 from fastapi import WebSocket, WebSocketDisconnect
 from services.chat_service import ChatSessionManager
 
 socket_router = fastapi.APIRouter()
 session_manager = ChatSessionManager()
+
+EMBEDDING_SERVICE_URL = "http://localhost:8000/embed"
 
 
 @socket_router.websocket("/ws/chat")
@@ -29,7 +32,11 @@ async def websocket_chat(websocket: WebSocket):
                         continue
 
                     session_manager.add_message(session_id, "user", user_query)
-                    response = f"Ваш запрос: {user_query}. Ожидайте ответа из клинических рекомендаций."
+
+                    # работа с моделью
+                    response = get_response(user_query)
+
+                    # response = f"Ваш запрос: {user_query}. Ожидайте ответа из клинических рекомендаций."
                     session_manager.add_message(session_id, "bot", response)
 
                     await websocket.send_text(
@@ -42,3 +49,26 @@ async def websocket_chat(websocket: WebSocket):
                 await websocket.send_text(json.dumps({"type": "error", "message": e}, ensure_ascii=False))
     except WebSocketDisconnect:
         session_manager.remove_session(session_id)
+
+
+def get_response(user_query):
+    try:
+        embed_request = {
+            "text": [user_query],
+            "task": "retrival.query",
+            "dimension": 1024
+        }
+        response = requests.post(EMBEDDING_SERVICE_URL, json=embed_request)
+        response.raise_for_status()
+        embedding_data = response.json()
+
+        embedding = embedding_data["embedding"][0]
+
+        response_text = f"Эмбеддинг получен. Размер: {len(embedding)}"
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при вызове embedding-service: {e}")
+        response_text = "Ошибка при генерации эмбеддинга"
+    except Exception as e:
+        print(f"Ошибка обработки эмбеддинга: {e}")
+        response_text = "Ошибка обработки"
+    return response_text
--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware # Yulia added this thing
 from api.router_socket import socket_router
 from api.router_page import page_router
 from docs_processing.upload_files import minzdrav_excel
 
+origins = ["*"] # Yulia added this thing
 
 app = FastAPI(title="Medical Support")
 app.include_router(socket_router)
 app.include_router(page_router)
+
+app.add_middleware( # Yulia added this thing |
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+) # Yulia added this thing ^
 
 # создание базы данных
 # minzdrav_excel()
